62161f729cb9b2a4865145d4d72c04a19a05e27d
Olga <olga1998senko@gmail.com>
2023-05-18 10:31:37 +0300

    hwObject IIFE

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 'use strict';
-const form = document.getElementById('form');
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    const object = {
-        email: formData.get('email'),
-        firstName: formData.get('firstName'),
-        lastName: formData.get('lastName'),
-        nickname: formData.get('nickname'),
-        password: formData.get('password'),
-        option: formData.get('option'),
-        message: formData.get('message'),
-        file: formData.get('file'),
-        terms: formData.get('terms')
-    }
-    console.log(object);
-})
+(function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const object = {
+            email: 'email',
+            firstName: 'firstName',
+            lastName: 'lastName',
+            nickname: 'nickname',
+            password: 'password',
+            option: 'option',
+            message: 'message',
+            file: 'file',
+            terms: 'terms'
+        }
+        console.log(object);
+    })
+})()
